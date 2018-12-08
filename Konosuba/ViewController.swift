5014fa3c4c31f4e905689430f728096fda93080d
Yusuke Hosonuma <tobi462@gmail.com>
2018-12-08 15:24:23 +0900

    Initial Commit

---
+++ Konosuba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Konosuba
+//
+//  Created by Yusuke Hosonuma on 2018/12/08.
+//  Copyright © 2018 Yusuke Hosonuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
